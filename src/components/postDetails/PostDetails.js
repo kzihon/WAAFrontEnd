298a83ab387071a5b95eec0b9e14848259dd29de
kzihon <kzihon@miu.edu>
2023-04-21 14:56:21 -0500

    lab9

--- src/components/postDetails/PostDetails.js
+++ src/components/postDetails/PostDetails.js
@@ -1,31 +1,31 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import Post from '../post'
+// import axios from 'axios'
+// import React, { useEffect } from 'react'
+// import Post from '../post'
 
-const POST_API_URL= 'http://localhost:8080/api/v1/posts'
+// const POST_API_URL= 'http://localhost:8080/api/v1/posts'
 
-const PostDetails = (props) => {
+// const PostDetails = (props) => {
 
-  const getPostById= (id)=>{
-  axios.get(POST_API_URL,"/", id)
-  .then(res=>{
-    console.log(res.data);
-  })
-  .catch(e=>{
-    console.log(e)
-  })
-}
+//   const getPostById= (id)=>{
+//   axios.get(POST_API_URL,"/", id)
+//   .then(res=>{
+//     console.log(res.data);
+//   })
+//   .catch(e=>{
+//     console.log(e)
+//   })
+// }
 
-getPostById(props.postId)
-  return (
-    <Post
-            title = {post.title}
-            author = {post.author}
-            id ={post.id}
-            key= {post.id}
+// getPostById(props.postId)
+//   return (
+//     <Post
+//             title = {post.title}
+//             author = {post.author}
+//             id ={post.id}
+//             key= {post.id}
           
-            />
-  )
-}
+//             />
+//   )
+// }
 
-export default PostDetails
+// export default PostDetails
